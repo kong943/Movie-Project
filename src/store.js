519eed8040cdf48b149c546076d1d0e8aed42255
kong943 <kakong209@gmail.com>
2025-01-21 14:27:52 +0700

    main

--- src/store.js
+++ src/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './Features/counter/couterSlice'
 import movieReducer from './Features/movie/movieSlice'
+import contactReducer from'./Features/contact/contactSlice'
+import ContactUs from './pages/ContactUs'
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     movie : movieReducer
+    Contact: contactReducer
   },
 })
